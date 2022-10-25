ccc1d9206891305f5a0b9743eb9bd6d4524b9be0
musaliyah <asmusaliar@gmail.com>
2022-10-24 20:47:55 -0700

    src-sw edits

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { request } = require('express');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -27,4 +28,19 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({request}) => request.destination === 'image',
+  new CacheFirst ({
+    cacheName: 'assets',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60
+      })
+    ]
+  })
+
+);
