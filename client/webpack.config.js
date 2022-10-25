c3c875ab2b26640ebb63326109b37cb3bf1acf30
musaliyah <asmusaliar@gmail.com>
2022-10-24 20:45:02 -0700

    webpack config'

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -54,9 +54,16 @@ module.exports = () => {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: babel-loader
-          }
-        }
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime'
+              ],
+            },
+          },
+        },
         
       ],
     },
